44e34ca963b09acd5f489e158168efda24ab8fe2
ahmetgurr <ahhmetgur@gmail.com>
2024-04-03 09:54:17 +0300

    splash screen

--- app/src/main/java/com/ahmetgur/pregnancytracker/MainActivity.kt
+++ app/src/main/java/com/ahmetgur/pregnancytracker/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -29,6 +30,7 @@ import com.ahmetgur.pregnancytracker.screen.login.ResetScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             val navController = rememberNavController()
             val authViewModel: AuthViewModel = viewModel()
